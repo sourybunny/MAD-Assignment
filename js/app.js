13f38ac3cd36549897ee32109250d5df1dadf753
sourya <soury.bunny@gmail.com>
2018-11-18 02:58:16 +0530

    chore: follow indentation and remove comments

--- js/app.js
+++ js/app.js
@@ -1,13 +1,10 @@
-
 var textarr = ["Single Application","Verified Avenues","Trusted Advisors","Informed Decisions"];
-// console.log(textarr);
 var count = 0;
 var heading = document.getElementById("heading");
-// console.log(heading);
 setInterval(animateText, 3000);
+
 function animateText() {
  heading.innerText = textarr[count];
- // console.log(heading.innerHTML);
     count++;
     if(count >= textarr.length)
     {
